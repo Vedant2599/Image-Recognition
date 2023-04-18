46896003dbe92ea0f68edfc677ea5561b4a616de
Vedant2599 <82651053+Vedant2599@users.noreply.github.com>
2023-04-18 16:56:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ Webcam.snap(function(data_uri){
 });}
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LqsmmT_cG/', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LqsmmT_cG/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log("Model is Loaded");
